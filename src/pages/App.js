c9c4daf4bbda9f6bf04129a3a6c05149bb9ea467
cedric <cedric@localhost.localdomain>
2019-07-11 22:55:31 -0400

    Commit to pull

--- src/pages/App.js
+++ src/pages/App.js
@@ -23,20 +23,24 @@ const App = ({ match }) => (
 );
 
 // Actual content for page
+const learn_img = require("./undraw_hello_aeia.svg")
 const AppPage = ({ match }) => (
-  <div>
-    App Page
-    <ul>
-      <li>
-        <Link to={`${match.url}/learn`}>Looking to learn</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/build`}>Looking to build</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/migrate`}>Looking to migrate</Link>
-      </li>
-    </ul>
+    <div class="app_page_container">
+      <div class="app_page_title">
+        <h1>What are you looking to do?</h1>
+      </div>
+      <ul class="app_page_ul">
+        <li class="app_page_li">
+          <img src={learn_img} height="10%" width="40%" style={{marginBottom: "100px" }}/>
+          <Link to={`${match.url}/learn`}>Looking to learn</Link>
+        </li>
+        <li class="app_page_li">
+          <Link to={`${match.url}/build`}>Looking to build</Link>
+        </li>
+        <li class="app_page_li">
+          <Link to={`${match.url}/migrate`}>Looking to migrate</Link>
+        </li>
+      </ul>
   </div>
 );
 
