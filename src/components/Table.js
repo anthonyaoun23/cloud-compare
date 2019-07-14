8520c06f74726cf63277cc5f0b396b2a5a89c4a4
KhangNguyen3000 <32154803+KhangNguyen3000@users.noreply.github.com>
2019-07-14 16:42:52 -0400

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -30,7 +30,7 @@ const rows = [
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-export default function SimpleTable() {
+export default function Table() {
   const classes = useStyles();
 
   return (
