a46fa3b490093e76992e490b344f762388ee8245
Anthony <anthony.aoun1@hotmail.com>
2019-07-15 11:19:58 -0400

    worked on migrate page

---
+++ src/components/SelectorsAnthony.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Checkbox from "@material-ui/core/Checkbox";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  formControl: {
+    margin: theme.spacing(3)
+  }
+}));
+
+export default function CheckboxesGroup() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    gilad: false,
+    jason: false,
+    antoine: false,
+    sps: false,
+    as: false,
+    sds: false
+  });
+
+  const handleChange = name => event => {
+    setState({ ...state, [name]: event.target.checked });
+  };
+
+  const { gilad, jason, antoine, sps, as, sds } = state;
+  const error = [gilad, jason, antoine].filter(v => v).length !== 2;
+
+  return (
+    <div className={classes.root}>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Server Assets</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={gilad}
+                onChange={handleChange("gilad")}
+                value="gilad"
+              />
+            }
+            label="Servers"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={jason}
+                onChange={handleChange("jason")}
+                value="jason"
+              />
+            }
+            label="Virtual Desktop Server"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={sps}
+                onChange={handleChange("sps")}
+                value="sps"
+              />
+            }
+            label="Software Platform Server"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={as}
+                onChange={handleChange("as")}
+                value="as"
+              />
+            }
+            label="Applications Server"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={sds}
+                onChange={handleChange("sds")}
+                value="sds"
+              />
+            }
+            label="Storage/ Data Server"
+          />
+        </FormGroup>
+        {/* <FormHelperText>Be careful</FormHelperText> */}
+      </FormControl>
+    </div>
+  );
+}
---
+++ src/components/SteppersAnthony.js
@@ -0,0 +1,169 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import StepContent from "@material-ui/core/StepContent";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import StepConnector from "@material-ui/core/StepConnector";
+import ListService from "./ListService";
+import TableKhang from "./TableKhang";
+import CheckboxesGroup from "../components/SelectorsAnthony";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "80%"
+  },
+  button: {
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1)
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing(2)
+  },
+  resetContainer: {
+    padding: theme.spacing(3)
+  },
+  icon: {
+    height: "60px",
+    width: "60px"
+  },
+  stepText: {
+    fontSize: "32px"
+  },
+  stepConnector: {
+    line: { marginLeft: "16px" }
+  }
+}));
+
+function getSteps() {
+  return [
+    "Select server asset(s)",
+    "Select service(s)",
+    "Compare service providers"
+  ];
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return (
+        <div>
+          <Typography>
+            {"Which server asset(s) would you like to bring to the cloud?"}
+          </Typography>
+          <CheckboxesGroup />
+        </div>
+      );
+    case 1:
+      return (
+        <div>
+          <Typography>
+            {"Which service(s) would you like to bring to the cloud?"}
+          </Typography>
+          <ListService />
+        </div>
+      );
+    case 2:
+      return (
+        <div>
+          <Typography>
+            {"These are the main providers of your picked cloud services"}
+          </Typography>
+          <TableKhang />
+        </div>
+      );
+    default:
+      return (
+        <Typography>
+          {"Wow we didn't expect that step! :) Please Refresh"}
+        </Typography>
+      );
+  }
+}
+
+export default function SteppersKhang() {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  function handleNext() {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  }
+
+  function handleBack() {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  }
+
+  function handleReset() {
+    setActiveStep(0);
+  }
+
+  const stepIconClass = {
+    classes: {
+      root: {
+        height: "60px",
+        width: "60px"
+      },
+      test: {
+        fontSize: "24px"
+      }
+    }
+  };
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className={classes.root}>
+        <Stepper activeStep={activeStep} orientation="vertical">
+          {steps.map((label, index) => (
+            <Step
+              key={label}
+              connector={<StepConnector className={classes.stepConnector} />}
+            >
+              <StepLabel
+                StepIconProps={{
+                  classes: { root: classes.icon }
+                }}
+                style={{ fontSize: "100px" }}
+                classes={{ label: classes.stepText }}
+              >
+                {label}
+              </StepLabel>
+              <StepContent>
+                {getStepContent(index)}
+                <div className={classes.actionsContainer}>
+                  <div>
+                    <Button
+                      disabled={activeStep === 0}
+                      onClick={handleBack}
+                      className={classes.button}
+                    >
+                      Back
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleNext}
+                      className={classes.button}
+                    >
+                      {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                    </Button>
+                  </div>
+                </div>
+              </StepContent>
+            </Step>
+          ))}
+        </Stepper>
+        {activeStep === steps.length && (
+          <Paper square elevation={0} className={classes.resetContainer}>
+            <Typography>All steps completed - you&apos;re finished</Typography>
+            <Button onClick={handleReset} className={classes.button}>
+              Reset
+            </Button>
+          </Paper>
+        )}
+      </div>
+    </div>
+  );
+}
