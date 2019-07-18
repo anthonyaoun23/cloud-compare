6fb487209b1def1b361a988b07613b98e2feb407
KhangNguyen3000 <nguyenh.khang.1999@gmail.com>
2019-07-17 22:32:23 -0400

    added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,6 +12,9 @@ selectText(){
     return <font color="red">Please make sure to include your name, a valid email address and the body. Thank you!</font>
   }else{
     return <font color="green">Thank you, we will get back to you as soon as we can!</font>
+    document.getElementById('outlined-required').reset();
+    document.getElementById('outlined-required2').reset();
+    document.getElementById('outlined-textarea').value = "";
   }
 }
 
@@ -28,7 +31,7 @@ onClickButton1 = () => {
       <h1>Found a bug or have a specific need? Contact us and we will help</h1>
       <ConsultationTextBox/>
       <p>{this.state.text}</p>
-      <button className="button button__accent" onClick={this.onClickButton1}>Send </button>
+      <button className="button button__accent" onClick={this.onClickButton1}>Send</button>
       </div>
     );
   }
