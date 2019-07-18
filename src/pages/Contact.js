d3135df8f77bb62e901fe84119d26a12d47272db
KhangNguyen3000 <nguyenh.khang.1999@gmail.com>
2019-07-17 20:22:58 -0400

    added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,8 +8,8 @@ class Contact extends React.Component {
 
 
 selectText(){
-  if(document.getElementById('outlined-required').value == "" || document.getElementById('outlined-required2').value == "" || document.getElementById('outlined-textarea').value == ""){
-    return <font color="red">Please make sure to include your name, email adress and the body. Thank you!</font>
+  if(document.getElementById('outlined-required').value == "" || document.getElementById('outlined-required2').value.includes("@") == false || document.getElementById('outlined-textarea').value == ""){
+    return <font color="red">Please make sure to include your name, a valid email address and the body. Thank you!</font>
   }else{
     return <font color="green">Thank you, we will get back to you as soon as we can!</font>
   }
