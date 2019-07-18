c6bf7f27657e0246243d36e50a6f8f97994735a4
Anthony <anthony.aoun1@hotmail.com>
2019-07-18 13:01:04 -0400

    worked on 404 page

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import error404 from "../images/undraw_page_not_found_su7k.svg";
+import { flexbox } from "@material-ui/system";
 
 const Page404 = () => (
-  <div>
-    <div>Error 404. We did not expect that :(</div>
-    <Link className="button button__accent" to="/">
+  <div
+    style={{
+      display: "flex",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: 'center',
+      flexDirection: "column",
+      height: '68vh'
+    }}
+  >
+    <div style={{ display: "inline-block" }}>
+      <img src={error404} class="expanded__image" style={{maxWidth: '600px'}} alt="undraw" />
+    </div>
+    <Link
+      className="button button__accent"
+      style={{ display: "inline-block", width: "20%", marginTop: '78px' }}
+      to="/"
+    >
       Back to Home page
     </Link>
   </div>
