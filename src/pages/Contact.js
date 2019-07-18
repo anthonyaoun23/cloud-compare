3ca8ca6ba3d17fc5454e086492b318da16677778
KhangNguyen3000 <nguyenh.khang.1999@gmail.com>
2019-07-17 20:18:37 -0400

    added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ConsultationTextBox from '../components/ConsultationTextBox';
 
-const Contact = () => (
-    <div>
-    <ConsultationTextBox/>
-    <button className="button button__accent" to="/app">Try now for free</button>
-    </div>
-)
+class Contact extends React.Component {
+  state = {
+  text: '',
+}
+
+
+selectText(){
+  if(document.getElementById('outlined-required').value == "" || document.getElementById('outlined-required2').value == "" || document.getElementById('outlined-textarea').value == ""){
+    return <font color="red">Please make sure to include your name, email adress and the body. Thank you!</font>
+  }else{
+    return <font color="green">Thank you, we will get back to you as soon as we can!</font>
+  }
+}
+
+onClickButton1 = () => {
+  this.setState({
+    text: this.selectText(),
+  });
+}
+
+  render() {
+    return (
+      <div>
+      <ConsultationTextBox/>
+      <p>{this.state.text}</p>
+      <button className="button button__accent" onClick={this.onClickButton1}>Send </button>
+      </div>
+    );
+  }
+}
 
 export default Contact;
