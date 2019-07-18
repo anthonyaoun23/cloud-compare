c45cade894b3cf66fe7e6a98e764c19c58edd38d
Anthony <anthony.aoun1@hotmail.com>
2019-07-17 21:45:02 -0400

    styled consultation page

--- src/components/ConsultationTextBox.js
+++ src/components/ConsultationTextBox.js
@@ -1,77 +1,90 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-        minWidth: 400,
+    display: "flex",
+
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+    // textAlign: 'center',
+    minWidth: 400,
+    maxWidth: "60%"
   },
   textField: {
     marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
+    marginRight: theme.spacing(1)
   },
   dense: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(2)
   },
   menu: {
-    width: 200,
- },
+    width: 200
+  }
 }));
 
 export default function ConsultationTextBox() {
   const classes = useStyles();
 
   return (
-    <form className={classes.container} noValidate autoComplete="off">
-    <TextField
-        required
-        id="outlined-required"
-        label="Name"
-        helperText="required*"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-    <TextField
-        required
-        id="outlined-required2"
-        label="Email"
-        helperText="required*"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-email-input"
-        label="Company name"
-        helperText="optional"
-        className={classes.textField}
-        type="email"
-        name="email"
-        autoComplete="email"
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-required"
-        label="Body"
-        helperText="required*"
-        multiline
-        style={{ margin: 10 }}
-        fullWidth
-        margin="none"
-        variant="outlined"
-        InputLabelProps={{
-          classes: {
-              input: classes.resize,
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <form className={classes.container} noValidate autoComplete="off">
+       <div style={{display: 'flex', width: '100%', justifyContent: 'space-between'}}>
+       <TextField
+       style={{width: '30%'}}
+          required
+          id="outlined-required"
+          label="Name"
+          helperText="required*"
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+       style={{width: '30%'}}
+
+          required
+          id="outlined-required2"
+          label="Email"
+          helperText="required*"
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+       style={{width: '30%'}}
+
+          id="outlined-email-input"
+          label="Company name"
+          helperText="optional"
+          className={classes.textField}
+          type="email"
+          name="email"
+          autoComplete="email"
+          margin="normal"
+          variant="outlined"
+        />
+       </div>
+        <TextField
+          id="outlined-required"
+          label="Body"
+          helperText="required*"
+          multiline
+          style={{ margin: 10 }}
+          fullWidth
+          margin="none"
+          variant="outlined"
+          InputLabelProps={{
+            classes: {
+              input: classes.resize
             },
-          shrink: true,
-        }}
-      />
-    </form>
+            shrink: true
+          }}
+        />
+      </form>
+    </div>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -23,7 +23,7 @@ onClickButton1 = () => {
 
   render() {
     return (
-      <div>
+      <div style={{textAlign: 'center', height: '100%'}}>
       <br/>
       <h1>Found a bug or have a specific need? Contact us and we will help</h1>
       <ConsultationTextBox/>
