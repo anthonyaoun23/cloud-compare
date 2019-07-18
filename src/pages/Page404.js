25361327fdb5767d08e5f0b218bd1256374eb86d
KhangNguyen3000 <32154803+KhangNguyen3000@users.noreply.github.com>
2019-07-18 11:44:39 -0400

    Update Page404.js

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Page404 = () => (
     <div>Error 404. We did not expect that :(</div>
-    <Link className="button button__accent" to="/app">Back to HomePage</Link>
+    <Link className="button button__accent" to="/index">Back to HomePage</Link>
 )
 
 export default Page404;
